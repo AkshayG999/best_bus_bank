9dc7ce3a2515e4985d73f59fec53ae62db3b61a9
Akshay-Gaikwad <akshay.gaikwad@nirmitee.io>
2024-02-03 14:09:00 +0530

    cors policy added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const userRoutes = require('./src/routes/userRoutes');
 const { sequelize } = require('./src/config/db');
 app.use(express.json());
+app.use(cors());
 
 
 
--- src/routes/groupRoute.js
+++ src/routes/groupRoute.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const groupController = require('../controllers/groupController')
+const groupController = require('../controllers/groupController');
+const { authenticateToken } = require("../middleware/authMid");
 
 
 router.post("/", groupController.createGroup)
-router.get("/", groupController.getGroups)
+router.get("/", authenticateToken, groupController.getGroups)
 
 
 module.exports = router
