009b2f854b780f37e423ddd6043011143d0404c3
Akshay-Gaikwad <akshay.gaikwad@nirmitee.io>
2024-04-11 21:11:14 +0530

    changes

--- userServices/routes/userRoutes.js
+++ userServices/routes/userRoutes.js
@@ -11,6 +11,11 @@ const { checkPermissionsMiddleware } = require("../../middlewareServices/permiss
 // router.put("/update/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.updateUser);
 // router.delete("/delete/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.deleteByID);
 
+// // User Routes
+// router.post("/login", userController.login);
+// router.get("/get-by-system-id/:systemID", authenticateToken, authorizeUser, userController.getBySystemID);
+// router.patch("/update-password/:systemID", authenticateToken, authorizeUser, userController.updatePassword);
+
 // Admin Routes
 router.post("/sign-up", userController.signUp);
 router.get("/get-all", userController.getAll);
@@ -18,10 +23,11 @@ router.put("/update/:systemID", userController.updateUser);
 router.delete("/delete/:systemID", userController.deleteByID);
 
 
+
 // User Routes
 router.post("/login", userController.login);
-router.get("/get-by-system-id/:systemID", authenticateToken, authorizeUser, userController.getBySystemID);
-router.patch("/update-password/:systemID", authenticateToken, authorizeUser, userController.updatePassword);
+router.get("/get-by-system-id/:systemID", userController.getBySystemID);
+router.patch("/update-password/:systemID", userController.updatePassword);
 
 
 module.exports = router;
