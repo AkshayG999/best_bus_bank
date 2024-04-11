9df9e4d25a63446bf1b530765360b78e115bc0c1
Akshay-Gaikwad <akshay.gaikwad@nirmitee.io>
2024-04-11 21:07:02 +0530

    changes

--- userServices/routes/userRoutes.js
+++ userServices/routes/userRoutes.js
@@ -5,11 +5,17 @@ const { authenticateToken, authorizeUser } = require("../../middlewareServices/a
 const { checkPermissionsMiddleware } = require("../../middlewareServices/permissionsMiddleware");
 
 
+// // Admin Routes
+// router.post("/sign-up", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.signUp);
+// router.get("/get-all", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', ""), userController.getAll);
+// router.put("/update/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.updateUser);
+// router.delete("/delete/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.deleteByID);
+
 // Admin Routes
-router.post("/sign-up", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.signUp);
-router.get("/get-all", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', ""), userController.getAll);
-router.put("/update/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.updateUser);
-router.delete("/delete/:systemID", authenticateToken, checkPermissionsMiddleware('18ec79c2611qtoxng', "", true), userController.deleteByID);
+router.post("/sign-up", userController.signUp);
+router.get("/get-all", userController.getAll);
+router.put("/update/:systemID", userController.updateUser);
+router.delete("/delete/:systemID", userController.deleteByID);
 
 
 // User Routes
